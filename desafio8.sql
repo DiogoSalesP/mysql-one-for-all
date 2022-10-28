82ab9ef1708cf8868b3ef51038c299bffd5eb5c8
Diogo Sales <diogosales.ads@gmail.com>
2022-10-28 00:53:05 -0300

    desafio8

---
+++ desafio8.sql
@@ -0,0 +1,7 @@
+SELECT
+	a.artista_name AS 'artista',
+    al.album_name AS 'album'
+FROM SpotifyClone.artistas AS a
+INNER JOIN SpotifyClone.albuns AS al ON a.artista_id = al.artista_id
+WHERE artista_name = 'Elis Regina'
+ORDER BY album;
